04a082665b14e55b568ea7c884785ff9f12fe9e7
Priyam Dua <duap@uwindsor.ca>
2022-05-24 14:30:08 -0400

    made some changes in controller

--- src/main/java/com/example/basicrepo/controller/MainController.java
+++ src/main/java/com/example/basicrepo/controller/MainController.java
@@ -3,6 +3,7 @@ package com.example.basicrepo.controller;
 
 import com.example.basicrepo.entities.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -10,11 +11,23 @@ import java.util.List;
 @RestController
 public class MainController {
 
+
+
+
+
     @GetMapping
     public List<Employee> getAllDetails()
     {
         return null;
     }
 
-    
+
+    @GetMapping(path="/v1/{id}")
+    public Employee getById(@PathVariable int id)
+    {
+        return null;
+    }
+
+
+
 }
