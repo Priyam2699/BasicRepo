3aae77cad36f06d14f506dcccc0f21441cf23249
Priyam Dua <duap@uwindsor.ca>
2022-05-24 14:31:38 -0400

    autowired Employee Repo

--- src/main/java/com/example/basicrepo/controller/MainController.java
+++ src/main/java/com/example/basicrepo/controller/MainController.java
@@ -1,7 +1,9 @@
 package com.example.basicrepo.controller;
 
 
+import com.example.basicrepo.Repo.EmployeeRepo;
 import com.example.basicrepo.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,10 @@ import java.util.List;
 
 @RestController
 public class MainController {
+
+    @Autowired
+    private EmployeeRepo employeeRepo;
+
 
     @GetMapping
     public List<Employee> getAllDetails()
