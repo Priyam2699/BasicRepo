b7c8f6dcd05b26404ec3df330f620f7289dffb9a
Priyam Dua <duap@uwindsor.ca>
2022-05-24 14:12:02 -0400

    First Commit

---
+++ src/main/java/com/example/basicrepo/BasicRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basicrepo/BasicRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
