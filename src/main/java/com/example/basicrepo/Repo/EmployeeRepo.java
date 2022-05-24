8c8df5a4bcf25dc7f57e040911d7bcfb0762621a
Priyam Dua <duap@uwindsor.ca>
2022-05-24 14:25:43 -0400

    Made Repo

---
+++ src/main/java/com/example/basicrepo/Repo/EmployeeRepo.java
@@ -0,0 +1,9 @@
+package com.example.basicrepo.Repo;
+
+import com.example.basicrepo.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
+}
